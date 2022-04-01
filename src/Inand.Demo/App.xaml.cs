d38281fac9c3acb7db4b79f209054314ae7069a4
levonskai <102808198+levonskai@users.noreply.github.com>
2022-04-01 21:02:01 +0300

    feat: add an empty solution
    The solution contains a library and a demo app.

---
+++ src/Inand.Demo/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace Inand.Demo
+{
+    public partial class App : Application
+    {
+    }
+}
---
+++ src/Inand/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+using System.Windows.Markup;
+
+[assembly: XmlnsPrefix("https://github.com/levonskai/Inand", "in")]
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None,
+    ResourceDictionaryLocation.SourceAssembly
+)]
