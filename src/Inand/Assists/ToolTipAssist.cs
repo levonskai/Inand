470432471221b23cc1a05c431b50bab233b8cdcf
levonskai <102808198+levonskai@users.noreply.github.com>
2022-04-09 15:44:34 +0300

    feat: add a default tooltip style
    ToolTipAssist has also been added.

---
+++ src/Inand/Assists/ToolTipAssist.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace Inand.Assists
+{
+    /// <summary>
+    /// Provides attached properties and events for setting the behavior of the tooltip.
+    /// </summary>
+    public static class ToolTipAssist
+    {
+        /// <summary>
+        /// Provides a method for custom positioning of the tooltip.
+        /// </summary>
+        public static CustomPopupPlacementCallback ToolTipPlacementCallback => PlaceToolTip;
+
+        private static CustomPopupPlacement[] PlaceToolTip(Size pSize, Size tSize, Point offset)
+        {
+            return new[]
+            {
+                new CustomPopupPlacement(new Point(tSize.Width/2 - pSize.Width/2, tSize.Height + 8), PopupPrimaryAxis.Horizontal)
+            };
+        }
+    }
+}
