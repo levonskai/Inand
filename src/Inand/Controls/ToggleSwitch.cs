7ad38440457f8ed204f1b60241041edde2b20c10
levonskai <102808198+levonskai@users.noreply.github.com>
2022-04-10 21:57:47 +0300

    feat: add toggle switch control

---
+++ src/Inand/Controls/ToggleSwitch.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace Inand.Controls
+{
+    /// <summary>
+    /// Represents a switch that can be toggled between two states.
+    /// </summary>
+    public class ToggleSwitch : ToggleButton
+    {
+        static ToggleSwitch()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ToggleSwitch), new FrameworkPropertyMetadata(typeof(ToggleSwitch)));
+        }
+    }
+}
